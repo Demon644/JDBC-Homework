fcb6b93aa54c8fc183a03efcc309f0e2785d8c34
Demon644 <demon6445@gmail.com>
2018-10-06 22:39:09 +0300

    7,8,9

--- src/Add.java
+++ src/Add.java
@@ -1,37 +1,38 @@
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class Add extends Program{
-	public void addCountry(String name) throws SQLException {
-		String query = "insert into country("
-				+ "name)"
-				+ " values (?);";
-		PreparedStatement pstmt = conn.prepareStatement(query);
-		pstmt.setString(1, name);
-		pstmt.executeUpdate();
-		pstmt.close();
-	}
-	public void addCity(String name) throws SQLException {
-		String query = "insert into city("
-				+ "name)"
-				+ " values (?);";
-		PreparedStatement pstmt = conn.prepareStatement(query);
-		pstmt.setString(1, name);
-		pstmt.executeUpdate();
-		pstmt.close();
-	}
-	
-	public void addHuman(String first_name, String last_name, int age) throws SQLException {
-		String query = "insert into human("
-				+ "first_name,"
-				+ "last_name,"
-				+ "age)"
-				+ " values (?,?,?);";
-		PreparedStatement pstmt = conn.prepareStatement(query);
-		pstmt.setString(1, first_name);
-		pstmt.setString(2, last_name);
-		pstmt.setInt(3, age);
-		pstmt.executeUpdate();
-		pstmt.close();
-	}
+public class Add extends Program {
+    public void addCountry(String name) throws SQLException {
+        String query = "insert into country("
+                + "name)"
+                + " values (?);";
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, name);
+        pstmt.executeUpdate();
+        pstmt.close();
+    }
+
+    public void addCity(String name) throws SQLException {
+        String query = "insert into city("
+                + "name)"
+                + " values (?);";
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, name);
+        pstmt.executeUpdate();
+        pstmt.close();
+    }
+
+    public void addHuman(String first_name, String last_name, int age) throws SQLException {
+        String query = "insert into human("
+                + "first_name,"
+                + "last_name,"
+                + "age)"
+                + " values (?,?,?);";
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, first_name);
+        pstmt.setString(2, last_name);
+        pstmt.setInt(3, age);
+        pstmt.executeUpdate();
+        pstmt.close();
+    }
 }
--- src/Show.java
+++ src/Show.java
@@ -4,53 +4,102 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Show extends Program {
-	
-	public void humansByName() throws SQLException {
-		String query = "select first_name,last_name,age from human order by first_name asc";
-		
-		PreparedStatement pstmt = conn.prepareStatement(query);
-		ResultSet rs = pstmt.executeQuery();
 
-		while(rs.next()) {
-			System.out.println(
-					"First name: " + rs.getString("first_name") + "\t |" +
-					"Last name: " + rs.getString("last_name") + "\t |" + 
-					"Age: " + rs.getInt("age") + "\t |"
-					);
-		}
-		pstmt.close();
-		rs.close();
-	}
-	
-	public void citiesByNameAsc() throws SQLException {
-		String query = "select name from city order by name desc";
-		
-		PreparedStatement pstmt = conn.prepareStatement(query);
-		ResultSet rs = pstmt.executeQuery();
-		
-		while(rs.next()) {
-			System.out.println(
-					"Name: " + rs.getString("name") + "\t |"
-					);
-		}
-		pstmt.close();
-		rs.close();
-		
-	}
-	
-	public void countries() throws SQLException {
-		String query = "select name from country";
-		
-		PreparedStatement pstmt = conn.prepareStatement(query);
-		ResultSet rs = pstmt.executeQuery();
-		
-		while(rs.next()) {
-			System.out.println(
-					"Name: " + rs.getString("name") + "\t |"
-					);
-		}
-		pstmt.close();
-		rs.close();
-	}
-	
+    public void allHumansByName() throws SQLException {
+        String query = "select first_name,last_name,age from human order by first_name asc";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(
+                    "First name: " + rs.getString("first_name") + "\t |" +
+                            "Last name: " + rs.getString("last_name") + "\t |" +
+                            "Age: " + rs.getInt("age") + "\t |"
+            );
+        }
+        pstmt.close();
+        rs.close();
+    }
+
+    public void citiesByNameAsc() throws SQLException {
+        String query = "select name from city order by name desc";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(
+                    "Name: " + rs.getString("name") + "\t |"
+            );
+        }
+        pstmt.close();
+        rs.close();
+
+    }
+
+    public void countries() throws SQLException {
+        String query = "select name from country";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(
+                    "Name: " + rs.getString("name") + "\t |"
+            );
+        }
+        pstmt.close();
+        rs.close();
+    }
+
+    public void humanById(int id) throws SQLException {
+        String query = "select * from human where idHuman = ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setInt(1, id);
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(
+                    "First name: " + rs.getString("first_name") + "\t |" +
+                            "Last name: " + rs.getString("last_name") + "\t |" +
+                            "Age: " + rs.getInt("age") + "\t |"
+            );
+        }
+        pstmt.close();
+        rs.close();
+    }
+
+    public void citiesById(int id) throws SQLException {
+        String query = "select * from city where idCity = ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setInt(1, id);
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(
+                    "Name: " + rs.getString("name") + "\t |"
+            );
+        }
+        pstmt.close();
+        rs.close();
+    }
+
+    public void countriesById(int id) throws SQLException {
+        String query = "select * from country where idCountry = ?";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setInt(1, id);
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(
+                    "Name: " + rs.getString("name") + "\t |"
+            );
+        }
+        pstmt.close();
+        rs.close();
+    }
 }
